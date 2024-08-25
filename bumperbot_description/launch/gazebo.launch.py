7a9bb2909c21d7e78f81f988ad15fbda22b2dcc3
Evgeny Korogodsky <emkorogodsky@gmail.com>
2024-08-25 01:04:54 -0500

    updated for new gazebo

--- bumperbot_description/launch/gazebo.launch.py
+++ bumperbot_description/launch/gazebo.launch.py
@@ -1,25 +1,66 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
 import os
+from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.parameter_descriptions import ParameterValue
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
 from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
 
 def generate_launch_description():
+    bumperbot_description = get_package_share_directory("bumperbot_description")
 
-    model_arg = DeclareLaunchArgument(
-        name="model",
-        default_value=os.path.join(get_package_share_directory("bumperbot_description"), "urdf", "bumperbot.urdf.xacro"),
-        description="Absolute path to robot urdf file"
+    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
+                                        bumperbot_description, "urdf", "bumperbot.urdf.xacro"
+                                        ),
+                                      description="Absolute path to robot urdf file"
     )
 
-    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]), value_type=str)
+    gazebo_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=[
+            str(Path(bumperbot_description).parent.resolve())
+            ]
+        )
+    
+    robot_description = ParameterValue(Command([
+            "xacro ",
+            LaunchConfiguration("model")
+        ]),
+        value_type=str
+    )
 
-    robot_state_publisher = Node(
+    robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         parameters=[{"robot_description": robot_description}]
     )
 
-    return LaunchDescription([])
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
+                launch_arguments=[
+                    ("gz_args", [" -v 4", " -r"]
+                    )
+                ]
+             )
+
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=["-topic", "robot_description",
+                   "-name", "bumperbot"],
+    )
+
+    return LaunchDescription([
+        model_arg,
+        gazebo_resource_path,
+        robot_state_publisher_node,
+        gazebo,
+        gz_spawn_entity,
+    ])
